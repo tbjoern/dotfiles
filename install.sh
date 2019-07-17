b26aa5e1f37c7c02642495f58365df289fdc663f
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2019-07-17 16:36:29 +0200

    Update install script with vim & tmux

--- install.sh
+++ install.sh
@@ -7,12 +7,19 @@ declare -a DEJAVU_FONTS=('DejaVu Sans Mono for Powerline.ttf' 'DejaVu Sans Mono 
 
 install -d $HOME/.fonts
 install -d $DOTFILES_DIR/.config
+install -d $HOME/.tmux/plugins
+install -d $HOME/.vim/autoload
+
+cp $DOTFILES_DIR/.vim/autoload/plug.vim $HOME/.vim/autoload
 
 ln -s $DOTFILES_DIR/.config $HOME/.config
 ln -s $DOTFILES_DIR/.zshrc $HOME/.zshrc
 ln -s $DOTFILES_DIR/.Xresources $HOME/.Xresources
 ln -s $DOTFILES_DIR/.Xdefaults $HOME/.Xdefaults
 ln -s $DOTFILES_DIR/.xinitrc $HOME/.xinitrc
+ln -s $DOTFILES_DIR/.vimrc $HOME/.vimrc
+ln -s $DOTFILES_DIR/.tmux.conf $HOME/.tmux.conf
+ln -s $DOTFILES_DIR/.tmux/plugins/tpm $HOME/.tmux/plugins/tpm
 
 for font in "${DEJAVU_FONTS[@]}"; do
     ln -s $DEJAVU_FONTS_DIR/$font $HOME/.fonts/$font
