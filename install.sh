1b2acc2394f16c0c881cc91a4eac138cc103f00b
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2019-07-17 17:17:46 +0200

    Make install script interactive

--- install.sh
+++ install.sh
@@ -1,3 +1,33 @@
+#! /bin/bash
+set -e
+
+# DRY_RUN=true
+
+function link {
+	if [[ ! ( (-f $1) || (-d $1 )) ]]; then
+		echo "File/Directory $1 does not exist!"
+		return 1
+	fi
+	if [[ -d $2 ]]; then
+		echo "Destination file $2 is an existing directory, skipping"
+		return 0
+	fi
+	if [[ -f $2 ]]; then
+		echo -n "Destination file $2 already exists, overwrite? y/[n]: "
+		read -n 1 overwrite
+		echo
+		if [[ ! (("$overwrite" = "y") || ("$overwrite" = "Y")) ]]; then
+			echo "Skipping $2"
+			return 0
+		fi
+	fi		
+	if [[ ! $DRY_RUN = true ]]; then
+		ln -s $1 $2
+	else
+		echo "Linking $1 to $2"
+	fi
+}
+
 git submodule init
 git submodule update
 
@@ -5,24 +35,24 @@ DOTFILES_DIR=$HOME/.dotfiles
 DEJAVU_FONTS_DIR=$DOTFILES_DIR/fonts/DejaVuSansMono
 declare -a DEJAVU_FONTS=('DejaVu Sans Mono for Powerline.ttf' 'DejaVu Sans Mono Bold for Powerline.ttf')
 
-install -d $HOME/.fonts
-install -d $DOTFILES_DIR/.config
-install -d $HOME/.tmux/plugins
-install -d $HOME/.vim/autoload
+install -dv $HOME/.fonts
+install -dv $DOTFILES_DIR/.config
+install -dv $HOME/.tmux/plugins
+install -dv $HOME/.vim/autoload
 
 cp $DOTFILES_DIR/.vim/autoload/plug.vim $HOME/.vim/autoload
 
-ln -s $DOTFILES_DIR/.config $HOME/.config
-ln -s $DOTFILES_DIR/.zshrc $HOME/.zshrc
-ln -s $DOTFILES_DIR/.Xresources $HOME/.Xresources
-ln -s $DOTFILES_DIR/.Xdefaults $HOME/.Xdefaults
-ln -s $DOTFILES_DIR/.Xdefaults.d $HOME/.Xdefaults.d
-ln -s $DOTFILES_DIR/.xinitrc $HOME/.xinitrc
-ln -s $DOTFILES_DIR/.vimrc $HOME/.vimrc
-ln -s $DOTFILES_DIR/.tmux.conf $HOME/.tmux.conf
-ln -s $DOTFILES_DIR/.tmux/plugins/tpm $HOME/.tmux/plugins/tpm
+link $DOTFILES_DIR/.config $HOME/.config
+link $DOTFILES_DIR/.zshrc $HOME/.zshrc
+link $DOTFILES_DIR/.Xresources $HOME/.Xresources
+link $DOTFILES_DIR/.Xdefaults $HOME/.Xdefaults
+link $DOTFILES_DIR/.Xresources.d $HOME/.Xresources.d
+link $DOTFILES_DIR/.xinitrc $HOME/.xinitrc
+link $DOTFILES_DIR/.vimrc $HOME/.vimrc
+link $DOTFILES_DIR/.tmux.conf $HOME/.tmux.conf
+link $DOTFILES_DIR/.tmux/plugins/tpm $HOME/.tmux/plugins/tpm
 
 for font in "${DEJAVU_FONTS[@]}"; do
-    ln -s $DEJAVU_FONTS_DIR/$font $HOME/.fonts/$font
+    link "$DEJAVU_FONTS_DIR/$font" "$HOME/.fonts/$font"
 done
 fc-cache -fv
