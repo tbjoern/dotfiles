dbb1492c379e55e1b694bcef921c43ecfc52ac27
Soeren Tietboehl <soeren@thinksono.com>
2021-12-15 10:31:19 +0100

    Fix headphones script

--- headphones.sh
+++ headphones.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [ -n "$HEADPHONES_BLE_ADDR" ]; then
+echo "$HEADPHONES_BLE_ADDR"
+
+if [ -z "$HEADPHONES_BLE_ADDR" ]; then
     echo "Headphones not configured. Set the HEADPHONES_BLE_ADDR env var to your headphones' bluetooth address."
-    return 1
+    exit 1
 fi
 
 if bluetoothctl info "$HEADPHONES_BLE_ADDR" | grep "Connected" | grep "yes" > /dev/null ; then
